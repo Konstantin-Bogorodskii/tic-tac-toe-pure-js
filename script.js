f44d4da03510445f2172d2f8b032efdb2aa417f4
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-08-03 23:46:55 +0300

    created layout and added styles

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('!!!');
+const status = document.querySelector('.status');
+const reset = document.querySelector('.reset');
+const display = document.querySelector('.game-grid');
+const cells = document.querySelectorAll('.grid-cell');
