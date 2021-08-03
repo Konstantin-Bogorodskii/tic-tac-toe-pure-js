1e95276c8321197f14e0e661fcc9942a92d8efac
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-08-04 00:30:34 +0300

    started to write app logic

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+// Variables
 const status = document.querySelector('.status');
 const reset = document.querySelector('.reset');
 const display = document.querySelector('.game-grid');
 const cells = document.querySelectorAll('.grid-cell');
+
+// Game Settings
+let gameIsAlive = true;
+let isNextX = true;
+
+// Functions
+
+const resetGame = e => {
+  console.log(e);
+};
+
+const cellClick = e => {
+  const target = e.target;
+  const position = target.getAttribute('data-position');
+};
+
+// Add Event Listeners
+reset.addEventListener('click', resetGame);
+cells.forEach(element => {
+  element.addEventListener('click', cellClick);
+});
